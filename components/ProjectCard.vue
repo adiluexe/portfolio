4584135e475e059d5ca05193a0e45e30f0056d43
adiluexe <adiluexe@gmail.com>
2025-07-05 22:20:07 +0800

    feat(works): implement works page with project listing and GSAP animations

---
+++ components/ProjectCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="group cursor-pointer w-full">
+    <div class="bg-primary w-full h-96 mb-4"></div>
+    <div class="flex justify-between items-center border-b border-text pb-2 tracking-tighter">
+      <h3 class="text-md font-bold uppercase">{{ project.name }}</h3>
+      <p class="text-sm uppercase">{{ project.stack }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    project: {
+      type: Object,
+      required: true
+    }
+  })
+</script>
--- pages/works.vue
+++ pages/works.vue
@@ -1,3 +1,39 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const { $gsap } = useNuxtApp()
+
+const worksContent = ref(null)
+
+const projects = ref([
+  { id: 1, name: 'PORTFOLIO', stack: 'NUXT.JS / VUE.JS / GSAP' },
+  { id: 2, name: 'HERONFIT', stack: 'FLUTTER / NEXT.JS / REACT / SUPABASE / FLASK' },
+  { id: 3, name: 'ALARP', stack: 'FLUTTER / SUPABASE / CUSTOMPAINT / FLUTTER3D' },
+  { id: 4, name: 'SNAPGRADE', stack: 'NEXT.JS / REACT / SPRINGBOOT' },
+  { id: 5, name: 'BATHALA', stack: 'GODOT' },
+  { id: 6, name: '[YOUR PROJECT HERE]', stack: '' }
+])
+
+onMounted(() => {
+  $gsap.from(worksContent.value.children, {
+    duration: 1,
+    y: 50,
+    opacity: 0,
+    stagger: 0.1,
+    ease: 'power4.out',
+  })
+})
+</script>
+
 <template>
-  <h1>Works</h1>
+  <div class="w-full min-h-screen flex flex-col justify-center text-center py-32 px-16">
+    <div ref="worksContent" class="w-full">
+      <h1 class="text-8xl font-normal uppercase tracking-tighter mb-16 font-satoshi"><span class="font-spice">W</span>orks</h1>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-8">
+        <NuxtLink v-for="project in projects" :key="project.id" :to="`/works/${project.id}`">
+          <ProjectCard :project="project" />
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>
