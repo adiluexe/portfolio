517fe57563bdb089503320d6f52f6111eb709fbd
adiluexe <adiluexe@gmail.com>
2025-07-05 22:28:52 +0800

    feat(playground): implement playground page with dynamic item listing and animations

---
+++ components/PlaygroundCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <component :is="playgroundItem.link ? 'NuxtLink' : 'div'" :to="playgroundItem.link ? `/playground/${playgroundItem.id}` : null" class="group cursor-pointer w-full">
+    <div class="bg-primary w-full aspect-[3/4] mb-4"></div>
+    <div class="border-b border-text pb-2 tracking-tighter">
+      <h3 class="text-md font-bold uppercase text-left">{{ playgroundItem.name }}</h3>
+    </div>
+  </component>
+</template>
+
+<script setup>
+  defineProps({
+    playgroundItem: {
+      type: Object,
+      required: true
+    }
+  })
+</script>
--- pages/playground.vue
+++ pages/playground.vue
@@ -1,3 +1,40 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const { $gsap } = useNuxtApp()
+
+const playgroundContent = ref(null)
+
+const playgroundItems = ref([
+  { id: 1, name: 'TRANGAZING', link: true },
+  { id: 2, name: 'WALTZ OF FOUR LEFT FEET', link: false },
+  { id: 3, name: 'TRANGAZING', link: true },
+  { id: 4, name: 'TRANGAZING', link: true },
+  { id: 5, name: 'TRANGAZING', link: false },
+  { id: 6, name: 'TRANGAZING', link: true },
+  { id: 7, name: 'TRANGAZING', link: false },
+  { id: 8, name: 'TRANGAZING', link: true },
+  { id: 9, name: 'TRANGAZING', link: false },
+])
+
+onMounted(() => {
+  $gsap.from(playgroundContent.value.children, {
+    duration: 1,
+    y: 50,
+    opacity: 0,
+    stagger: 0.1,
+    ease: 'power4.out',
+  })
+})
+</script>
+
 <template>
-  <h1>Playground</h1>
+  <div class="w-full min-h-screen flex flex-col justify-center text-center py-32 px-16">
+    <div ref="playgroundContent" class="w-full">
+      <h1 class="text-8xl font-normal uppercase tracking-tighter mb-16 font-satoshi"><span class="font-spice">P</span>layground</h1>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-x-8 gap-y-8">
+        <PlaygroundCard v-for="item in playgroundItems" :key="item.id" :playgroundItem="item" />
+      </div>
+    </div>
+  </div>
 </template>
