1bb86164d89db97c6d7333574bfcddebcf6d2d27
adiluexe <adiluexe@gmail.com>
2025-07-12 12:42:41 +0800

    feat: implement GSAP ScrollSmoother and ScrollTrigger for enhanced scrolling effects

--- app.vue
+++ app.vue
@@ -1,9 +1,26 @@
 <template>
-  <div class="font-satoshi bg-background text-text">
+  <div id="smooth-wrapper" class="font-satoshi bg-background text-text">
     <TheHeader />
     <TheNavigation />
-    <main class="min-h-screen">
+    <main id="smooth-content" class="min-h-screen">
       <NuxtPage />
     </main>
   </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue';
+const gsap = useNuxtApp().$gsap;
+const ScrollSmoother = useNuxtApp().$ScrollSmoother;
+
+onMounted(() => {
+  if (ScrollSmoother) {
+    ScrollSmoother.create({
+      wrapper: '#smooth-wrapper',
+      content: '#smooth-content',
+      smooth: 1.2,
+      effects: true,
+    });
+  }
+});
+</script>
--- plugins/gsap.client.ts
+++ plugins/gsap.client.ts
@@ -1,5 +1,10 @@
 import { gsap } from "gsap";
+import ScrollSmoother from "gsap/ScrollSmoother";
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.provide("gsap", gsap);
+  nuxtApp.provide("ScrollSmoother", ScrollSmoother);
+  nuxtApp.provide("ScrollTrigger", ScrollTrigger);
 });
