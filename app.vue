d7ed30e1bc474178723e069da393bd399059c341
adiluexe <adiluexe@gmail.com>
2025-07-16 16:10:11 +0800

    feat: add close menu functionality to TheHeader component for improved navigation

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="smooth-wrapper" class="font-satoshi bg-background text-text">
-    <TheHeader :menuOpen="menuOpen" @toggle-menu="menuOpen = !menuOpen" />
+    <TheHeader :menuOpen="menuOpen" @toggle-menu="menuOpen = !menuOpen" @close-menu="menuOpen = false" />
     <TheNavigation :menuOpen="menuOpen" @close-menu="menuOpen = false" />
     <main id="smooth-content" class="min-h-screen">
       <NuxtPage />
